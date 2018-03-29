0599143c42e6432d70d034e63c8501115cafaf14
Elle <10051455+BatmanAMA@users.noreply.github.com>
2018-03-29 14:13:09 -0500

    add 'devicemgmt' features

--- module/Public/test.ps1
+++ module/Public/test.ps1
@@ -1,22 +1,33 @@
+[CmdletBinding()]
 Param (
 
-        # List of custom properties
-        [Parameter()]
-        [string[]]
-        $CustomProperties,
+    # List of custom properties
+    [Parameter()]
+    [string[]]
+    $CustomFields,
 
-        #List of properties to lookup
-        [parameter()]
-        [hashtable]
-        $Lookup,
+    #List of properties to lookup
+    [parameter()]
+    [hashtable]
+    $Lookup,
 
-        # you can specify properties as arguments to this command
-        [Parameter(ValueFromRemainingArguments=$true)]
-        $Properties
-    )
+    # you can specify properties as arguments to this command
+    [Parameter(ValueFromRemainingArguments = $true)]
+    $Properties
+)
 
-    $object = @{}
-    for ($i = 0; $i -lt $Properties.Count; $i+=2) {
-        $object[($Properties[$i] -replace '-' -replace ':')] = $Properties[$i+1]
+$object = @{custom_fields = @{}}
+for ($i = 0; $i -lt $Properties.Count; $i += 2) {
+    $Name = $Properties[$i] -replace '-' -replace ':'
+    $value = $Properties[$i + 1]
+    if ($name -in $lookup.keys) {
+        #$value = ConvertTo-nbID -source $value -value $name
     }
-    New-Object -TypeName psobject -Property $object
+    if ($name -in $CustomFields) {
+        $object.custom_fields[$name] = $value
+    }
+    else {
+        $object[$name] = $value
+    }
+}
+$object = New-Object -TypeName psobject -Property $object
