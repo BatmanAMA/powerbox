488c9573e9ef2aa4e1dad97df5dab9e2887c3042
Cook <usrcoo@corp.tds.local>
2018-04-25 08:32:53 -0500

    Fix a few bugs

--- test/nbx.tests.ps1
+++ test/nbx.tests.ps1
@@ -1,20 +1,39 @@
 Describe 'Set wrapper functions' {
+    $token = ConvertTo-SecureString -String "APITOKEN" -AsPlainText -Force
+    Connect-nbAPI -APIurl 'http://example.com' -Token $token
 
-    foreach ($function in (get-Command -Verb Set -Module powerbox | Where-Object Name -ne 'Set-nbObject'))
-    {
-        Mock -CommandName Set-NbObject -MockWith {} -Verifiable -ModuleName powerbox -ParameterFilter {
-            ($Resource -replace '^[^\/]*/' -replace '-') -eq ($function.Name -replace 'Set-Nb')
+    $testCases = get-Command -Verb Set -Module powerbox |
+        Where-Object Name -ne 'Set-nbObject' |
+        ForEach-Object {
+        @{
+            function     = $function.Name
+            resourcename = ($function.Name -replace 'Set-Nb')
         }
-        &($function.Name) | should -Not -Throw
+    }
+    Mock Invoke-nbApi -MockWith {}
+    it "should map <function> to <resourcename>" -TestCases $testCases {
+        param(
+            $function,
+            $resourceName
+        )
+        Mock -CommandName Set-NbObject -MockWith {} -Verifiable -ParameterFilter {
+            ($Resource -replace '^[^\/]*/' -replace '-') -eq $resourceName
+        }
+        {&$function -Id 0} | should -Not -Throw
+        Assert-VerifiableMock
     }
 }
 Describe 'New wrapper functions' {
-
+    $token = ConvertTo-SecureString -String "APITOKEN" -AsPlainText -Force
+    Connect-nbAPI -APIurl 'http://example.com' -Token $token
     foreach ($function in (get-Command -Verb New -Module powerbox)) {
-        Mock -CommandName New-NbObject -MockWith {} -Verifiable -ModuleName powerbox -ParameterFilter {
+        Mock -CommandName New-NbObject -MockWith {} -Verifiable -ParameterFilter {
             ($Resource -replace '^[^\/]*/' -replace '-') -eq ($function.Name -replace 'New-Nb')
         }
-        &($function.Name) | should -Not -Throw
+        Mock Invoke-nbApi -MockWith {}
+        it "$($function.Name) should work" {
+            {&($function.Name)} | should -Not -Throw
+            Assert-VerifiableMock
+        }
     }
 }
-
