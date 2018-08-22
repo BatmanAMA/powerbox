4f3d626c3ce7405cd8afcccfcb2e5878f8357829
Elle <10051455+BatmanAMA@users.noreply.github.com>
2018-08-22 14:16:47 -0500

    v2.1.1 - 22.Aug.2018
    Patch for Get-NB* to work for objects with no custom fields.

--- debugHarness.ps1
+++ debugHarness.ps1
@@ -2,8 +2,8 @@
 Remove-Module -Name powerbox -ErrorAction SilentlyContinue
 Import-Module -Name $PSScriptRoot\module\powerbox.psd1
 #$token = Read-Host -AsSecureString -Prompt "API Token"
-$apiurl = "https://ankana.tds.local/api"
+$apiurl = "https://netbox.contoso.com/api"
 #Connect-nbAPI -Token $token -APIUrl $apiurl
 
-Get-nbVirtualMachine #-Query @{name='CRDS01'} -APIurl $apiurl
-#Get-nbObject -Resource virtualization/virtual-machines -APIUrl $apiurl -Query @{name='CRDS01'}
+Get-nbPlatform -APIUrl $apiurl
+#Get-nbObject -Resource virtualization/virtual-machines -APIUrl $apiurl
